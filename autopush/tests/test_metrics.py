dce4c3810636ed91fe1f227c5031207ee0086312
Jeremiah Orem <oremj@oremj.com>
2015-04-07 14:46:43 -0700

    tests/metrics: add prefix to unit tests

--- autopush/tests/test_metrics.py
+++ autopush/tests/test_metrics.py
@@ -27,15 +27,15 @@ class TwistedMetricsTestCase(unittest.TestCase):
 class DatadogMetricsTestCase(unittest.TestCase):
     @patch("autopush.metrics.datadog")
     def test_basic(self, mock_dog):
-        m = DatadogMetrics("someapikey", "someappkey")
+        m = DatadogMetrics("someapikey", "someappkey", namespace="testpush")
         ok_(len(mock_dog.mock_calls) > 0)
         m._client = Mock()
         m.start()
         m._client.start.assert_called_with(flush_interval=10,
                                            roll_up_interval=10)
         m.increment("test", 5)
-        m._client.increment.assert_called_with("test", 5)
+        m._client.increment.assert_called_with("testpush.test", 5)
         m.gauge("connection_count", 200)
-        m._client.gauge.assert_called_with("connection_count", 200)
+        m._client.gauge.assert_called_with("testpush.connection_count", 200)
         m.timing("lifespan", 113)
-        m._client.timing.assert_called_with("lifespan", value=113)
+        m._client.timing.assert_called_with("testpush.lifespan", value=113)
