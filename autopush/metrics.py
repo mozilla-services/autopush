b2e7a15e106a1bea3c758d79207aea65f5ff2ec2
Jeremiah Orem <oremj@oremj.com>
2015-04-07 14:43:56 -0700

    metrics/datadog: add namespace option

--- autopush/metrics.py
+++ autopush/metrics.py
@@ -27,22 +27,30 @@ class TwistedMetrics(object):
 
 
 class DatadogMetrics(object):
-    def __init__(self, api_key, app_key, flush_interval=10):
+    def __init__(self, api_key, app_key, flush_interval=10,
+                 namespace="autopush"):
+
         datadog.initialize(api_key=api_key, app_key=app_key)
         self._client = ThreadStats()
         self._flush_interval = flush_interval
-
         self._host = get_hostname()
+        self._namespace = namespace
+
+    def _prefix_name(self, name):
+        return "%s.%s" % (self._namespace, name)
 
     def start(self):
         self._client.start(flush_interval=self._flush_interval,
                            roll_up_interval=self._flush_interval)
 
     def increment(self, name, count=1, **kwargs):
-        self._client.increment(name, count, host=self._host, **kwargs)
+        self._client.increment(self._prefix_name(name), count, host=self._host,
+                               **kwargs)
 
     def gauge(self, name, count, **kwargs):
-        self._client.gauge(name, count, host=self._host, **kwargs)
+        self._client.gauge(self._prefix_name(name), count, host=self._host,
+                           **kwargs)
 
     def timing(self, name, duration, **kwargs):
-        self._client.timing(name, value=duration, host=self._host, **kwargs)
+        self._client.timing(self._prefix_name(name), value=duration,
+                            host=self._host, **kwargs)
