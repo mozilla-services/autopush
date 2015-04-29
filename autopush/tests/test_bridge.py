c34d266919690c11cb1db7ed5ba678a63cd5f80b
Ben Bangert <ben@groovie.org>
2015-04-29 15:49:17 -0700

    Fixup tests to run under cpython ok.

--- autopush/tests/test_bridge.py
+++ autopush/tests/test_bridge.py
@@ -1,7 +1,4 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+# -*- coding: utf-8 -*-
 from mock import Mock, patch
 from moto import mock_dynamodb2
 from unittest import TestCase
