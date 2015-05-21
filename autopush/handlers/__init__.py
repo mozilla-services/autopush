6d6200422364a61392497c840322c2bc7a09f410
Ben Bangert <ben@groovie.org>
2015-05-21 11:57:32 -0700

    Add the handlers package

---
+++ autopush/handlers/__init__.py
@@ -0,0 +1,7 @@
+"""Custom Notification Handlers
+
+This package contains custom notification handlers for other services that
+may have notifications relayed to, or custom wake-up signals sent should a
+device be offline.
+
+"""
