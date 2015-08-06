cff67f291246b2d1e7c613e56d9d8e02fc60bb72
Ben Bangert <ben@groovie.org>
2015-08-05 18:24:57 -0700

    Add psutil for proper killing of child procs. Setup basic running websocket listener.

--- autopush/tests/test_integration.py
+++ autopush/tests/test_integration.py
@@ -1,26 +1,66 @@
 import os
+import signal
 import subprocess
 from unittest.case import SkipTest
 
+import boto
+import psutil
 from twisted.trial import unittest
+from twisted.internet import reactor
 
-
+here_dir = os.path.abspath(os.path.dirname(__file__))
+root_dir = os.path.dirname(os.path.dirname(here_dir))
 moto_process = None
 
 
 def setUp():
-    if "SKIP_INTEGRATION" in os.environ:
+    boto_path = os.path.join(root_dir, "automock", "boto.cfg")
+    boto.config.load_from_path(boto_path)
+    if "SKIP_INTEGRATION" in os.environ:  # pragma: nocover
         raise SkipTest("Skipping integration tests")
     global moto_process
     cmd = "moto_server dynamodb2 -p 5000"
-    moto_process = subprocess.Popen(cmd)
+    moto_process = subprocess.Popen(cmd, shell=True, env=os.environ)
 
 
 def tearDown():
     global moto_process
-    moto_process.terminate()
+    # This kinda sucks, but its the only way to nuke the child procs
+    proc = psutil.Process(pid=moto_process.pid)
+    child_procs = proc.children(recursive=True)
+    for p in [proc] + child_procs:
+        os.kill(p.pid, signal.SIGTERM)
+    moto_process.wait()
+
+    # Clear out the boto config that was loaded so the rest of the tests run
+    # fine
+    for section in boto.config.sections():
+        boto.config.remove_section(section)
 
 
 class TestIntegration(unittest.TestCase):
+    def setUp(self):
+        from autobahn.twisted.websocket import WebSocketServerFactory
+        from autopush.settings import AutopushSettings
+        from autopush.websocket import SimplePushServerProtocol
+        settings = AutopushSettings(
+            hostname="localhost",
+            statsd_host=None,
+        )
+        factory = WebSocketServerFactory("ws://localhost:9010/")
+        factory.protocol = SimplePushServerProtocol
+        factory.protocol.ap_settings = settings
+        factory.setProtocolOptions(
+            webStatus=False,
+            maxFramePayloadSize=2048,
+            maxMessagePayloadSize=2048,
+            openHandshakeTimeout=5,
+        )
+        settings.factory = factory
+        self.websocket = reactor.listenTCP(9010, factory)
+
+    def tearDown(self):
+        self.websocket.stopListening()
+
     def test_basic(self):
         pass
