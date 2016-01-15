ae706b6cbb44e891adc12284a771c598a3acdbbb
jrconlin <jconlin+git@mozilla.com>
2016-01-15 11:52:38 -0800

    Coverage fix

--- autopush/tests/test_main.py
+++ autopush/tests/test_main.py
@@ -180,7 +180,7 @@ class EndpointMainTestCase(unittest.TestCase):
         message_write_throughput = 0
         senderid_list = '{"12345":{"auth":"abcd"}}'
         s3_bucket = "none"
-        key_hash = ""
+        key_hash = "supersikkret"
         senderid_expry = 0
 
     def setUp(self):
