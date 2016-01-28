4da257cfc98f3ef6d2c177e3bf83e0742dcb8e8a
jrconlin <jconlin+git@mozilla.com>
2016-01-28 13:21:57 -0800

    more r's

--- autopush/router/simple.py
+++ autopush/router/simple.py
@@ -98,13 +98,13 @@ class SimpleRouter(object):
             try:
                 result = yield self._send_notification(uaid, node_id,
                                                        notification)
-            except (ConnectError, UserError, ConnectionRefusedError) as exp:
+            except (ConnectError, UserError, ConnectionRefusedError) as exc:
                 self.metrics.increment("updates.client.host_gone")
                 dead_cache.put(node_key(node_id), True)
                 yield deferToThread(router.clear_node,
                                     uaid_data).addErrback(self._eat_db_err)
-                if type(exp) is ConnectionRefusedError:
-                    log.err("Could not route message: %s" % repr(exp))
+                if isinstance(exc, ConnectionRefusedError):
+                    log.err("Could not route message: %s" % repr(exc))
                 raise RouterException("Node was invalid", status_code=503,
                                       response_body="Retry Request",
                                       log_exception=False, errno=202)
@@ -155,11 +155,11 @@ class SimpleRouter(object):
             returnValue(self.stored_response(notification))
         try:
             result = yield self._send_notification_check(uaid, node_id)
-        except (ConnectError, UserError, ConnectionRefusedError), exp:
+        except (ConnectError, UserError, ConnectionRefusedError) as exc:
             self.metrics.increment("updates.client.host_gone")
             dead_cache.put(node_key(node_id), True)
-            if isinstance(exp, ConnectionRefusedError):
-                log.err("Could not route message: %s" % repr(exp))
+            if isinstance(exc, ConnectionRefusedError):
+                log.err("Could not route message: %s" % repr(exc))
             yield deferToThread(
                 router.clear_node,
                 uaid_data).addErrback(self._eat_db_err)
