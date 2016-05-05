63ff016c3102e516d1966c964d054ce4ec2b17d1
jrconlin <jconlin+git@mozilla.com>
2016-05-05 13:51:30 -0700

    fix: Check connected month bounds for preflight
    Add full checks for preflight failure conditions.
    
    Closes #461

--- autopush/tests/test_router.py
+++ autopush/tests/test_router.py
@@ -757,14 +757,15 @@ class WebPushRouterTestCase(unittest.TestCase):
         d.addBoth(verify_deliver)
         return d
 
-    def test_route_with_invalid_channel_id(self):
+    def test_route_with_missing_month(self):
         self.agent_mock.request.return_value = response_mock = Mock()
         response_mock.addCallback.return_value = response_mock
         type(response_mock).code = PropertyMock(
             side_effect=MockAssist([202, 200]))
         self.message_mock.store_message.return_value = True
         self.message_mock.all_channels.return_value = (True, [])
-        router_data = dict(node_id="http://somewhere", uaid=dummy_uaid)
+        router_data = dict(node_id="http://somewhere",
+                           uaid=dummy_uaid)
         self.router_mock.get_uaid.return_value = router_data
         self.router.message_id = uuid.uuid4().hex
 
@@ -773,7 +774,54 @@ class WebPushRouterTestCase(unittest.TestCase):
         def verify_deliver(fail):
             exc = fail.value
             ok_(exc, RouterException)
-            eq_(exc.status_code, 404)
+            eq_(exc.status_code, 410)
+            self.flushLoggedErrors()
+        d.addBoth(verify_deliver)
+        return d
+
+    def test_route_with_invalid_channel_id(self):
+        self.agent_mock.request.return_value = response_mock = Mock()
+        response_mock.addCallback.return_value = response_mock
+        type(response_mock).code = PropertyMock(
+            side_effect=MockAssist([202, 200]))
+        self.message_mock.store_message.return_value = True
+        self.message_mock.all_channels.return_value = (True, [])
+        month = self.router.ap_settings.current_msg_month
+        router_data = dict(node_id="http://somewhere",
+                           current_month=month,
+                           uaid=dummy_uaid)
+        self.router_mock.get_uaid.return_value = router_data
+        self.router.message_id = uuid.uuid4().hex
+
+        d = self.router.route_notification(self.notif, router_data)
+
+        def verify_deliver(fail):
+            exc = fail.value
+            ok_(exc, RouterException)
+            eq_(exc.status_code, 410)
+            self.flushLoggedErrors()
+        d.addBoth(verify_deliver)
+        return d
+
+    def test_route_with_invalid_month(self):
+        self.agent_mock.request.return_value = response_mock = Mock()
+        response_mock.addCallback.return_value = response_mock
+        type(response_mock).code = PropertyMock(
+            side_effect=MockAssist([202, 200]))
+        self.message_mock.store_message.return_value = True
+        self.message_mock.all_channels.return_value = (True, [])
+        router_data = dict(node_id="http://somewhere",
+                           current_month="invalid_month",
+                           uaid=dummy_uaid)
+        self.router_mock.get_uaid.return_value = router_data
+        self.router.message_id = uuid.uuid4().hex
+
+        d = self.router.route_notification(self.notif, router_data)
+
+        def verify_deliver(fail):
+            exc = fail.value
+            ok_(exc, RouterException)
+            eq_(exc.status_code, 410)
             self.flushLoggedErrors()
         d.addBoth(verify_deliver)
         return d
