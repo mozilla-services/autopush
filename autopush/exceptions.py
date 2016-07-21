b405cce663ad0e245ff020b142e4af6f37389034
Philip Jenvey <pjenvey@underboss.org>
2016-07-21 15:22:27 -0700

    fix: kill mutable default arguments
    closes #541

--- autopush/exceptions.py
+++ autopush/exceptions.py
@@ -12,8 +12,8 @@ class InvalidTokenException(Exception):
 class InvalidRequest(AutopushException):
     """Invalid request exception, may include custom status_code and message
     to write for the error"""
-    def __init__(self, message, status_code=400, errno=None, headers={}):
+    def __init__(self, message, status_code=400, errno=None, headers=None):
         super(AutopushException, self).__init__(message)
         self.status_code = status_code
         self.errno = errno
-        self.headers = headers
+        self.headers = {} if headers is None else headers
