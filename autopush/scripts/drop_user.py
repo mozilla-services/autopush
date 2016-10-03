9e5a95f9004046e5195273c3d4ab7b12eaa7f63c
Ben Bangert <ben@groovie.org>
2016-10-03 16:33:34 -0700

    feat: add user record cleanup script (#677)
    Add's a new drop_user command that scans the AccessIndex for
    users that haven't connected in the given month and removes the
    route records.
    
    Closes #645

---
+++ autopush/scripts/drop_user.py
@@ -0,0 +1,39 @@
+import time
+
+import click
+
+from autopush.db import (
+    get_router_table,
+    Router,
+)
+from autopush.metrics import SinkMetrics
+
+
+@click.command()
+@click.option('--router_table_name', help="Name of the router table.")
+@click.option('--months-ago', default=2, help="Months ago to remove.")
+@click.option('--batch_size', default=25,
+              help="Deletes to run before pausing.")
+@click.option('--pause_time', default=1,
+              help="Seconds to pause between batches.")
+def drop_users(router_table_name, months_ago, batch_size, pause_time):
+    router_table = get_router_table(router_table_name)
+    router = Router(router_table, SinkMetrics())
+
+    click.echo("Deleting users with a last_connect %s months ago."
+               % months_ago)
+
+    count = 0
+    for deletes in router.drop_old_users(months_ago):
+        click.echo("")
+        count += deletes
+        if count >= batch_size:
+            click.echo("Deleted %s user records, pausing for %s seconds."
+                       % pause_time)
+            time.sleep(pause_time)
+            count = 0
+    click.echo("Finished old user purge.")
+
+
+if __name__ == '__main__':  # pragma: nocover
+    drop_users()
