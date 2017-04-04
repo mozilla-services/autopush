3576d2075b590861950b984160243de35bf982d5
jrconlin <jconlin+git@mozilla.com>
2017-04-04 14:00:53 -0700

    bug: Limit exception trapping to known types when processing crypto-key
    closes #860

--- autopush/tests/test_integration.py
+++ autopush/tests/test_integration.py
@@ -959,6 +959,18 @@ class TestWebPush(IntegrationBase):
         yield self.shut_down(client)
 
     @inlineCallbacks
+    def test_basic_delivery_with_invalid_vapid_ckey(self):
+        data = str(uuid.uuid4())
+        client = yield self.quick_register(use_webpush=True)
+        vapid_info = _get_vapid()
+        vapid_info['crypto-key'] = "invalid|"
+        yield client.send_notification(
+            data=data,
+            vapid=vapid_info,
+            status=401)
+        yield self.shut_down(client)
+
+    @inlineCallbacks
     def test_delivery_repeat_without_ack(self):
         data = str(uuid.uuid4())
         client = yield self.quick_register(use_webpush=True)
