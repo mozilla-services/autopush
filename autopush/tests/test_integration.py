0d2435566598af35d3583467a26d82016259fe53
jr conlin <me+crypt@jrconlin.com>
2017-04-19 08:32:50 -0700

    bug: Do not report InvalidSignature as unhandled
    closes #872

--- autopush/tests/test_integration.py
+++ autopush/tests/test_integration.py
@@ -974,6 +974,20 @@ class TestWebPush(IntegrationBase):
         yield self.shut_down(client)
 
     @inlineCallbacks
+    def test_basic_delivery_with_invalid_signature(self):
+        data = str(uuid.uuid4())
+        client = yield self.quick_register(use_webpush=True)
+        vapid_info = _get_vapid(
+            payload={"aud": "https://pusher_origin.example.com",
+                     "sub": "mailto:admin@example.com"})
+        vapid_info['auth'] = vapid_info['auth'][:-3] + "bad"
+        yield client.send_notification(
+            data=data,
+            vapid=vapid_info,
+            status=401)
+        yield self.shut_down(client)
+
+    @inlineCallbacks
     def test_basic_delivery_with_invalid_vapid_ckey(self):
         data = str(uuid.uuid4())
         client = yield self.quick_register(use_webpush=True)
