7e3a735bdc53c390d681c6281b142c4a7a50a0b6
Philip Jenvey <pjenvey@underboss.org>
2017-08-24 11:48:25 -0700

    set a ping interval default for autopush_rs, for now

--- autopush/main.py
+++ autopush/main.py
@@ -338,7 +338,7 @@ class RustConnectionApplication(AutopushMultiService):
                 cert=ns.router_ssl_cert,
                 dh_param=ns.ssl_dh_param
             ),
-            auto_ping_interval=ns.auto_ping_interval,
+            auto_ping_interval=ns.auto_ping_interval or 5,  # XXX: for autopush_rs
             auto_ping_timeout=ns.auto_ping_timeout,
             max_connections=ns.max_connections,
             close_handshake_timeout=ns.close_handshake_timeout,
