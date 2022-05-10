5a778e021bc91e65591d8175d02c63e871fefb73
jrconlin <jr+git@mozilla.com>
2022-05-10 11:28:39 -0700

    feat: add apns error reason to the metric reporting
    Closes #1457

--- autopush/router/apnsrouter.py
+++ autopush/router/apnsrouter.py
@@ -163,7 +163,7 @@ class APNSRouter(object):
             elif isinstance(e, (HTTP20Error, socket.error)):
                 reason = "http2_error"
             else:
-                reason = "unknown"
+                reason = e.extra.get("reason", "unknown")
             if isinstance(e, RouterException) and e.status_code in [404, 410]:
                 raise RouterException(
                     str(e),
